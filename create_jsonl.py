d6fc37b5b1e379cc09731f7afb4adb468be9a0ac
Brian <brian.salkas@gmail.com>
2023-02-15 08:35:50 -0600

    add option for user to specify whether they want to only scrape questions

--- create_jsonl.py
+++ create_jsonl.py
@@ -13,38 +13,46 @@ reddit = praw.Reddit(
 parser = argparse.ArgumentParser()
 parser.add_argument('--prompt-end', default='', help='only use submission titles that end with PROMPT_END')
 parser.add_argument('--comments', type=int, help='use first-level comments as completions')
-parser.add_argument('-s','--subreddits', required=True, )
+parser.add_argument('-s','--subreddits', required=True, help='requires a + seperated list of subreddits')
+parser.add_argument('--questions-only',action='store_true', help='only scrape propmpts that end in a questionmark')
 args = parser.parse_args()
 # end   argparse stuff
+PROMPT_END='\n\n###\n\n'
+COMP_END='.#,'
+if args.questions_only:
+    PROMPT_END = '?'
 for sub in args.subreddits.split('+'):
     next_sub = reddit.subreddit(sub)
     for submission in next_sub.top(limit=1_000):
-        if submission.title.endswith('?'):
-            title = submission.title.strip()
-            title = title.replace('"', "'")
-            title = title.replace('?','')
-            title = title.replace("\\",'')
-            selftext = submission.selftext.strip()
-            selftext = selftext.replace('\n',' ')
-            selftext = selftext.replace('"', "'")
-            selftext = selftext.replace("\\",'')
-            if selftext:
-                string = f'"prompt": "{title}?", "completion": " {selftext},#."'
+        selftext = submission.selftext
+        title = submission.title.strip()
+        if args.questions_only and not title.endswith('?'):
+            continue
+        if args.questions_only:
+            title = title.replace('?','') # remove PROMPT_END
+        title = title.replace('"', "'") # convert " to '
+        title = title.replace("\\",'') # remove escape chars
+        selftext = selftext.replace('\n',' ') # remove newlines in completion
+        selftext = selftext.replace('"', "'")
+        selftext = selftext.replace("\\",'')
+        selftext = submission.selftext.strip()
+        if selftext:
+            string = f'"prompt": "{title}{PROMPT_END}", "completion": " {selftext}{COMP_END}"'
+            string = '{' + string +  '}'
+            print(string)
+        if args.comments:
+            comment_count = 0
+            for comment in submission.comments:
+                if comment_count > args.comments:
+                    break
+                try:
+                    selftext = comment.body.strip()
+                except Exception:
+                    break
+                selftext = selftext.replace('\n',' ')
+                selftext = selftext.replace('"', "'")
+                selftext = selftext.replace('\\','')
+                string = f'"prompt": "{title}{PROMPT_END}", "completion": " {selftext}{COMP_END}"'
                 string = '{' + string +  '}'
                 print(string)
-            if args.comments:
-                comment_count = 0
-                for comment in submission.comments:
-                    if comment_count > args.comments:
-                        break
-                    try:
-                        selftext = comment.body.strip()
-                    except Exception:
-                        break
-                    selftext = selftext.replace('\n',' ')
-                    selftext = selftext.replace('"', "'")
-                    selftext = selftext.replace('\\','')
-                    string = f'"prompt": "{title}?", "completion": " {selftext},#."'
-                    string = '{' + string +  '}'
-                    print(string)
-                    comment_count += 1
+                comment_count += 1
