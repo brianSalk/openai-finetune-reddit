ee1a238011cd67ef8a89eab782988ed388367b72
Brian <brian.salkas@gmail.com>
2023-03-05 16:44:33 -0600

    add if name main to create_jsonl

--- create_jsonl.py
+++ create_jsonl.py
@@ -20,56 +20,58 @@ parser.add_argument('-s', '--subreddits', required=True, help='requires a + sepa
 parser.add_argument('--questions-only', action='store_true', help='only scrape prompts that end in a question mark')
 args = parser.parse_args()
 
-PROMPT_END = '\n\n###\n\n'
-COMP_END = '.#,'
-if args.questions_only:
-    PROMPT_END = '?'
+def create():
+    PROMPT_END = '\n\n###\n\n'
+    COMP_END = '.#,'
+    if args.questions_only:
+        PROMPT_END = '?'
 
-# Loop through subreddits and submissions
-for sub in args.subreddits.split('+'):
-    next_sub = reddit.subreddit(sub)
-    for submission in next_sub.top(limit=1000):
-        title = submission.title.strip()
-        selftext = submission.selftext.strip()
+    # Loop through subreddits and submissions
+    for sub in args.subreddits.split('+'):
+        next_sub = reddit.subreddit(sub)
+        for submission in next_sub.top(limit=1000):
+            title = submission.title.strip()
+            selftext = submission.selftext.strip()
 
-        # Skip if title does not end with question mark
-        if args.questions_only and not title.endswith('?'):
-            continue
+            # Skip if title does not end with question mark
+            if args.questions_only and not title.endswith('?'):
+                continue
 
-        # Remove question mark from title
-        if args.questions_only:
-            title = title[:-1]
+            # Remove question mark from title
+            if args.questions_only:
+                title = title[:-1]
 
-        # Replace special characters
-        title = title.replace('"', "'")
-        title = title.replace("\\", '')
-        selftext = selftext.replace('\n', ' ')
-        selftext = selftext.replace('"', "'")
-        selftext = selftext.replace("\\", '')
+            # Replace special characters
+            title = title.replace('"', "'")
+            title = title.replace("\\", '')
+            selftext = selftext.replace('\n', ' ')
+            selftext = selftext.replace('"', "'")
+            selftext = selftext.replace("\\", '')
 
-        # Generate JSON string
-        if selftext:
-            prompt = f'"prompt": "{title}{PROMPT_END}",'
-            completion = f'"completion": " {selftext}{COMP_END}"'
-            string = '{' + prompt + completion + '}'
-            print(string)
-
-        # Generate JSON string for comments if args.comments specified
-        if args.comments:
-            comment_count = 0
-            for comment in submission.comments:
-                if comment_count > args.comments:
-                    break
-                try:
-                    selftext = comment.body.strip()
-                except Exception:
-                    break
-                selftext = selftext.replace('\n', ' ')
-                selftext = selftext.replace('"', "'")
-                selftext = selftext.replace('\\', '')
+            # Generate JSON string
+            if selftext:
                 prompt = f'"prompt": "{title}{PROMPT_END}",'
                 completion = f'"completion": " {selftext}{COMP_END}"'
                 string = '{' + prompt + completion + '}'
                 print(string)
-                comment_count += 1
 
+            # Generate JSON string for comments if args.comments specified
+            if args.comments:
+                comment_count = 0
+                for comment in submission.comments:
+                    if comment_count > args.comments:
+                        break
+                    try:
+                        selftext = comment.body.strip()
+                    except Exception:
+                        break
+                    selftext = selftext.replace('\n', ' ')
+                    selftext = selftext.replace('"', "'")
+                    selftext = selftext.replace('\\', '')
+                    prompt = f'"prompt": "{title}{PROMPT_END}",'
+                    completion = f'"completion": " {selftext}{COMP_END}"'
+                    string = '{' + prompt + completion + '}'
+                    print(string)
+                    comment_count += 1
+if __name__ == "__main__":
+    create()
