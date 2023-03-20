39d8e5fc33b3b99c72731b589a035998771ecd86
Brian <brian.salkas@gmail.com>
2023-03-20 15:19:20 -0500

    added minimum upvotes for comments, submission, and added must_contain string (which is not yet supported

--- create_jsonl.py
+++ create_jsonl.py
@@ -12,11 +12,10 @@ reddit = praw.Reddit(
     user_agent=credentials.user_agent
 )
 
-
-
 def create(subreddits,comments,submission_body,
         questions_only,min_completion_length,
-        max_completion_length,submissions_per_sub):
+        max_completion_length,submissions_per_sub,
+        must_contain, min_rating_for_sub, min_rating_for_comment):
     PROMPT_END = '\n\n###\n\n'
     COMP_END = '.#,'
     if questions_only:
@@ -44,9 +43,9 @@ def create(subreddits,comments,submission_body,
             selftext = selftext.replace('"', "'")
             selftext = selftext.replace("\\", '')
 
-            # Generate JSON string
-            if selftext and submission_body and \
-                len(selftext) > min_completion_length and len(selftext) < max_completion_length:
+            # Generate JSON string using submission body
+            if selftext and submission_body and submission and len(selftext) >= min_completion_length and len(selftext) <= max_completion_length \
+                and submission.score >= min_rating_for_sub:
                 prompt = f'"prompt": "{title}{PROMPT_END}",'
                 completion = f'"completion": " {selftext}{COMP_END}"'
                 string = '{' + prompt + completion + '}'
@@ -67,11 +66,12 @@ def create(subreddits,comments,submission_body,
                     prompt = f'"prompt": "{title}{PROMPT_END}",'
                     completion = f'"completion": " {selftext}{COMP_END}"'
                     string = '{' + prompt + completion + '}'
-                    if len(selftext) < max_completion_length and \
-                    len(selftext) > min_completion_length:
+                    if len(selftext) <= max_completion_length and \
+                    len(selftext) >= min_completion_length and comment.score >= min_rating_for_comment:
                         ans.append(string)
                         comment_count += 1
     return "\n".join(ans)
+
 if __name__ == "__main__":
 	# Set up argparse
 	parser = argparse.ArgumentParser()
@@ -83,7 +83,10 @@ if __name__ == "__main__":
 	parser.add_argument('--min_completion_length',type=int, help='set mininum length of a completion')
 	parser.add_argument('--max_completion_length',type=int, help='set maximum completion length')
 	parser.add_argument('--submissions_per_sub',type=int,default=1_000, help='number of submissions per subreddit to scrape (depending on which other parameters you select you may get much fewer than the number you enter for this argmuent)')
+    parser.add_argument('--min_rating_for_sub', type=int, default=0,help='minimum number of upvotes for submission to be considered for jsonl')
+    parser.add_argument('--min_rating_for_comment', type=int, default=0, help='min number of upvotes for comment to be considered for jsonl')
 	args = parser.parse_args()
 	create(args.subreddits,args.comments,args.submission_body,
         args.questions_only,args.min_completion_length,
-        args.max_completion_length,args.submissions_per_sub)
+        args.max_completion_length,args.submissions_per_sub,
+        "NOT SUPPORTED", args.min_rating_for_sub, args.min_rating_for_comment)
